f9426ab324d03037dd44959107f5fff353184434
Олег <kryroekoleso@gmail.com>
2021-12-15 22:16:43 +0200

    test

--- HW 3 .py
+++ HW 3 .py
@@ -1,38 +1,32 @@
 from hwpl import plates_list
 
-
-
 unique_numbers = set(plates_list)
 print(len(unique_numbers))
 
 
+
 search = input("please input namber : ")
 
-#suma = 0
-
-
-#while search > 0:
- #   digit = search % 10
-  #  suma = suma + digit
-   # search = search // 10
-
-#print(suma)
-
-
-#search = str(input("please input namber : "))
-
-x = search
-
-
+x = search.upper()
 
 if x in plates_list:
     print("true")
 else:
     print("false")
-#    print("Yes")
-#else:
-#    print("No")
 
+# a = int(search[2])
+# b = int(search[3])
+# c = int(search[4])
+# d = int(search[5])
+#
+# print(a+b+c+d)
 
+x = x[2:6]
+
+e = 0
+for i in range(2, 6):
+    e = e + int(x[i])
+
+#print(x)
 
 exit()
