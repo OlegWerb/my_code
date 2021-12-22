cdbdb6ef256cee2c1e6580b7659059ca83da5f48
Олег <kryroekoleso@gmail.com>
2021-12-22 12:41:42 +0200

    test

--- HW 4.py
+++ HW 4.py
@@ -7,6 +7,20 @@ def uniq_numb():
 
 uniq_numb()
 
+search = input("please input namber : ")
+
+nam_plates = search
+
+def in_plates(nam_plates):
+    if len(nam_plates) != 8:
+        return False
+    else:
+        if nam_plates in plates_list:
+            nam_plates = nam_plates[:2], nam_plates[2], nam_plates[3], nam_plates[5], nam_plates[6:]
+            return nam_plates
+
+print(in_plates(nam_plates))
+
 def search_plates():
     search = input("please input namber : ")
 
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+from hwpl import plates_list
 
+search = input("please input namber : ")
 
+nam_plates = search
 
+def in_plates(nam_plates):
+    if len(nam_plates) != 8:
+        return False
+    else:
+        if nam_plates in plates_list:
+            nam_plates = nam_plates[:2], nam_plates[2], nam_plates[3], nam_plates[5], nam_plates[6:]
+            return nam_plates
 
+print(in_plates(nam_plates))
+        # else:
 
+    # search = input("please input namber : ")
 
 
 
